90c6f651a201fd83242cb24adc301e467151f3ab
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2023-02-13 09:52:52 -0500

    Start coding

---
+++ src/main/java/com/epam/rd/boundedbuffer/App.java
@@ -0,0 +1,13 @@
+package com.epam.rd.boundedbuffer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
