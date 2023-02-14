875c84320144fefbb1c05736d4ad43241b82390c
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2023-02-14 13:53:15 -0500

    Adding Properties to low-level Solution

--- src/main/java/com/epam/rd/boundedbuffer/App.java
+++ src/main/java/com/epam/rd/boundedbuffer/App.java
@@ -1,8 +1,41 @@
 package com.epam.rd.boundedbuffer;
 
 import java.util.Random;
+import java.util.Properties;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class App  {
+public class App {
+    private static final String SLEEP_PRODUCER = "Sleep_Producer";
+    private static final String SLEEP_CONSUMER = "Sleep_Consumer";
+
+    private static Properties prop = new Properties();
+
+    static {
+	boolean readFile = false;
+	File propFile = new File("ProduceConsumer.prop");
+	if (propFile.exists()) {
+	    try {
+		FileReader fr = new FileReader(propFile);
+		prop.load(fr);
+		readFile = true;
+	    } catch (FileNotFoundException fe) {
+	    } catch (IOException ioe) {
+	    }
+	}
+
+	if (!readFile) {
+	    prop.setProperty(SLEEP_CONSUMER, "6000");
+	    prop.setProperty(SLEEP_PRODUCER, "5000");
+	}
+    }
+
+    private static int getProp(String propName) {
+	return Integer.parseInt(prop.getProperty(propName));
+    }
+
     public static void main( String[] args ) {
 	Random random = new Random(System.currentTimeMillis());
 
@@ -15,7 +48,7 @@ public class App  {
 		boundedBuffer.put((char) i);
 		i = (((i + 1) - 'A') % ('z' - 'A' + 1)) + 'A';
 		try {
-		    Thread.sleep(random.nextInt(5000));
+		    Thread.sleep(random.nextInt(getProp(SLEEP_PRODUCER) + 1));
 		} catch(InterruptedException ie) { }
 	    }
 	};
@@ -25,7 +58,7 @@ public class App  {
 		char value = boundedBuffer.get();
 		System.out.println("Value: " + value);
 		try {
-		    Thread.sleep(random.nextInt(6000));
+		    Thread.sleep(random.nextInt(getProp(SLEEP_CONSUMER) + 1));
 		} catch (InterruptedException ie) { }
 	    }
 	};
