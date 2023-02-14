35cb2e3648e51871d2d59547dfa03267201cf925
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2023-02-14 12:41:05 -0500

    Adding Properties to non-solution

--- src/main/java/com/epam/rd/boundedbuffer/App.java
+++ src/main/java/com/epam/rd/boundedbuffer/App.java
@@ -1,12 +1,42 @@
 package com.epam.rd.boundedbuffer;
 
 import java.util.Random;
-/**
- * 
- *
- */
-public class App  {
-    public static void main( String[] args ) {
+import java.util.Properties;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class App {
+    private static final String SLEEP_PRODUCER = "Sleep_Producer";
+    private static final String SLEEP_CONSUMER = "Sleep_Consumer";
+    
+    private static Properties prop = new Properties();
+
+    static {
+	boolean readFile = false;
+	File propFile = new File("ProduceConsumer.prop");
+	if (propFile.exists()) {
+	    try {
+		FileReader fr = new FileReader(propFile);
+		prop.load(fr);
+		readFile = true;
+	    } catch (FileNotFoundException fe) {
+	    } catch (IOException ioe) {
+	    }
+	}
+
+	if (!readFile) {
+	    prop.setProperty(SLEEP_CONSUMER, "6000");
+	    prop.setProperty(SLEEP_PRODUCER, "5000");
+	}
+    }
+
+    private static int getProp(String propName) {
+	return Integer.parseInt(prop.getProperty(propName));
+    }
+    
+    public static void main(String[] args) {
 	Random random = new Random(System.currentTimeMillis());
 	
 	BoundedBuffer<Integer> boundedBuffer = new BoundedBufferSimpleImpl<Integer>(Integer.class, 10);
@@ -15,7 +45,7 @@ public class App  {
 	    while (true) {
 		boundedBuffer.put(i++);
 		try {
-		    Thread.sleep(random.nextInt(5000));
+		    Thread.sleep(random.nextInt(getProp(SLEEP_PRODUCER) + 1));
 		} catch(InterruptedException ie) { }
 	    }
 	};
@@ -24,7 +54,7 @@ public class App  {
 		int value = boundedBuffer.get();
 		System.out.println("Value: " + value);
 		try {
-		    Thread.sleep(random.nextInt(6000));
+		    Thread.sleep(random.nextInt(getProp(SLEEP_CONSUMER) + 1));
 		} catch (InterruptedException ie) { }
 	    }
 	};
